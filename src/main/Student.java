8c8f5022b29b705c9be7152ed34270339c9d02e7
Harshalmandale3 <harshalmandale37@gmail.com>
2023-03-15 13:08:07 +0530

    MyFirst Commit

---
+++ src/main/Student.java
@@ -0,0 +1,47 @@
+package main;
+
+public class Student {
+	
+	private int roll;
+	private String name;
+	private Address address;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Student(String name, Address address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+	public Student(Address address) {
+		super();
+		this.address = address;
+	}
+
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Student(int roll) {
+		super();
+		this.roll = roll;
+	}
+
+	public Student(int roll, String name, Address address) {
+		super();
+		this.roll = roll;
+		this.name = name;
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
